703a5464b1ca98251989aa1af94c165cfc696bcd
JuanDavid2021 <davidyk_15@hotmail.com>
2022-05-05 15:32:38 -0500

    pokeapp

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
-const sequelize = 
+let sequelize = 
  process.env.NODE_ENV === "production"?
 new Sequelize({
    database: DB_NAME,
@@ -32,10 +32,6 @@ new Sequelize({
     {logging:false, native:false}
   )
 
-/* `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-} */
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
